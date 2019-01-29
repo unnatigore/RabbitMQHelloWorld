16674e4c276b5660550f5496a79fa96a08fcac00
GORE <=unnatigore345@gmail.com>
2019-01-29 16:29:46 +0530

    Adds all rabbitmq assignements files

---
+++ RabbitMQSender/src/main/java/com/capgemini/app/controller/SenderController.java
@@ -0,0 +1,32 @@
+package com.capgemini.app.controller;
+
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/message")
+
+public class SenderController {
+
+	@Autowired
+	private RabbitMessagingTemplate template;
+	
+	@Bean
+	public Queue queue() {
+		return new Queue("helloworld", false);
+	}
+
+	@GetMapping("/{message}")
+	public void message(@PathVariable String message) {
+		
+		template.convertAndSend("helloworld",message);
+		
+	}
+}
